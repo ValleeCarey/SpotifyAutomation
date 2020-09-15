52e9452fc605368cdb36abd5e87b85c97829ef1e
Vallee Carey <valleecarey@Vallees-MBP.home>
2020-09-15 00:14:13 -0400

    Added youtube credentials and logic

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
+from youtube_client import YoutubeClient
+
 def run():
-    pass
+    youtube_client = YoutubeClient('./credits/client_id.json')
+    playlists = youtube_client.get_playlists()
+
+
+    for index, playlist in enumerate(playlists):
+        print(f"{index}: {playlist.title}")
+    choice = int(input("Enter your choice: "))
+    chosen_playlist = playlists[choice] 
+    print(f"You selected: {chosen_playlist.title} ")
+
+
 
 if __name__ == '__main__':
     run()
