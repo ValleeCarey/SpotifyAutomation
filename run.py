e17ea128d6ca35d97e1ff0bf20f6c8fa384df8a2
Vallee Carey <valleecarey@Vallees-MBP.home>
2020-09-15 00:36:51 -0400

    added spotify song search

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
+import os
+
 from youtube_client import YoutubeClient
 
 def run():
     #get a list of playlist from youtube 
     youtube_client = YoutubeClient('./credits/client_id.json')
+    spotify_client = SpotifyClient(os.getenv('SPOTIFY_AUTH_TOKEN'))
     playlists = youtube_client.get_playlists()
 
 # ask which playlist we want to get the music video from  
@@ -18,6 +21,13 @@ songs = youtube_client.get_videos_from_playlist(chosen_playlist.id)
 print(f"Attempting to add {len(songs)}")
 
 #search for spotify song 
+
+for song in songs: 
+    spotify_song_id = spotify_client.search_song(song.artist, song.track)
+    if spotify_song_id:
+     added_song = spotify_client.add_song_to_spotify(spotify_song_id)
+     if added_song:
+         print(f"Added {song.artist}")
 
 
 
