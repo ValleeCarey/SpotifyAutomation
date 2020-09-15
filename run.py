26d14e0400ba85ee5e05b62570ff64dc534fd5b4
Vallee Carey <valleecarey@Vallees-MBP.home>
2020-09-15 00:18:44 -0400

    added song info for each video

--- run.py
+++ run.py
@@ -1,15 +1,23 @@
 from youtube_client import YoutubeClient
 
 def run():
+    #get a list of playlist from youtube 
     youtube_client = YoutubeClient('./credits/client_id.json')
     playlists = youtube_client.get_playlists()
 
-
+# ask which playlist we want to get the music video from  
     for index, playlist in enumerate(playlists):
         print(f"{index}: {playlist.title}")
     choice = int(input("Enter your choice: "))
     chosen_playlist = playlists[choice] 
     print(f"You selected: {chosen_playlist.title} ")
+
+#for each video in the playlist get song info from Youtube
+
+songs = youtube_client.get_videos_from_playlist(chosen_playlist.id)
+print(f"Attempting to add {len(songs)}")
+
+#search for spotify song 
 
 
 
